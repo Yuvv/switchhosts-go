a029d86cd3bf718fc43ffd427bc57b0d680395a1
Yuvv <yuvvyuvv@outlook.com>
2020-01-14 00:20:27 +0800

    fix linux funcs bugs

--- funcs_linux.go
+++ funcs_linux.go
@@ -9,9 +9,9 @@ import (
 
 func OpenFileWithDefaultEditor(filename string) error {
 	editorCommand := os.Getenv("EDITOR")
-	if editCommand == emptyString {
-		editCommand = os.Getenv("VISUAL")
-		if editCommand == emptyString {
+	if editorCommand == emptyString {
+		editorCommand = os.Getenv("VISUAL")
+		if editorCommand == emptyString {
 			return errors.New("you doesn't have set any editor, please add `EDITOR` or `VISUAL` to your configuration")
 		}
 	}
